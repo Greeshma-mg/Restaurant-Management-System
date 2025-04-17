17b173d67e6ad2a9516244742c6021b3577dce81
Greeshma-mg <greeshmamgnair@gmail.com>
2025-04-17 19:03:28 +0530

    updated  backend add razorpay route

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,16 +3,12 @@ const { processPayment, getPayments, updatePayment, deletePayment } = require(".
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-// Create a new payment
 router.post("/", protect, processPayment);
 
-// Get all payments
 router.get("/", getPayments);
 
-// Update a payment by ID
 router.patch("/:id", updatePayment);
 
-// Delete a payment by ID
 router.delete("/:id", deletePayment);
 
 module.exports = router;
